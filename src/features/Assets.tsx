d83cb5eb57a04af4791dcf873efdaa9b607e45f0
loaialaddien <loai.alaa@fixedmea.com>
2021-10-15 13:29:26 +0200

    handle loading error

--- src/features/Assets.tsx
+++ src/features/Assets.tsx
@@ -3,18 +3,34 @@ import { search } from '../apis/imagesApi'
 import Asset,{ AssetType } from '../components/asset/asset';
 import { Input, } from 'antd';
 const { Search } = Input;
+const contentBasedOnState= ({error,loading,assets}:{error:Boolean,loading:Boolean,assets:Array<AssetType>})=>{
+    if(error) return "error please try again";
+    if(loading) return "loading";
+    if(assets.length === 0) return "no data";
+    return assets.map((asset:AssetType)=>{
+        const id = asset.data[0].nasa_id;
+        return <Asset key={id} asset={asset}></Asset>
+    })
+}
 export default function Assets() {
     const [assets,setAssets] = useState<AssetType[]>([]);
+    const [error,setError] = useState(false)
+    const [loading,setLoading] = useState(false)
     const fetchAssets = async (query="milkyway")=>{
-        const {data} = await search(query);
-        // no need for limiting the list  since I don't do any more requests
-        const onlyDataWithImages = (data as any)?.collection?.items.filter(showImageType)
-        setAssets(onlyDataWithImages);
+        if(error) setError(false);
+        setLoading(true);
+        search(query).then(({data}:any)=>{
+            // no need for limiting the list  since I don't do any more requests
+            const onlyDataWithImages = (data as any)?.collection?.items.filter(showImageType)
+            setAssets(onlyDataWithImages);
+        }).catch(err=>setError(true))
+        .finally(()=>setLoading(false));
 
     }
     useEffect(()=>{
         fetchAssets();
     },[])
+
     return (
         <div>
             <div style={{position:"fixed",top:0,left:0,right:0,zIndex:100}}>
@@ -28,10 +44,9 @@ export default function Assets() {
             />
             </div>
             <div style={{marginTop:50}}>
-                {assets.map((asset:AssetType)=>{
-                    const id = asset.data[0].nasa_id;
-                    return <Asset key={id} asset={asset}></Asset>
-                })}
+            {
+                contentBasedOnState({error,assets,loading})
+            }
             </div>
         </div>
     )
